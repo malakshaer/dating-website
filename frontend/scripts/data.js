204071f3b875c85a044a3b2248234b1a6270804b
malakshaer <shaermk06@gmail.com>
2022-10-04 19:39:46 +0300

    frontend:  get favorites api url added

--- frontend/scripts/data.js
+++ frontend/scripts/data.js
@@ -1,4 +1,8 @@
-const getUsersAPI = "";
+const getUsersAPI = "http://127.0.0.1:8000/api/auth/getUsers";
+
+const getFavoriteAPI = "http://127.0.0.1:8000/api/auth/getFavorite";
 
 let users = [];
 axios.get(getUsersAPI).then((res) => (users = res.data));
+
+axios.get(getFavoriteAPI).then((res) => (users = res.data));
