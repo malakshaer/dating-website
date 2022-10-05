ac3c13c4385f272e09a4252a888fe29d907142d2
malakshaer <shaermk06@gmail.com>
2022-10-05 21:15:47 +0300

    backend: all routes added

--- server/routes/api.php
+++ server/routes/api.php
@@ -12,16 +12,23 @@ Route::group(["prefix" => "auth"], function () {
         Route::get("/logout", [LandingController::class, "logout"]);
         Route::get("/refresh", [LandingController::class, "refresh"]);
         Route::get("/getUsers", [LandingController::class, "getUsers"]);
-        Route::get("/getFavorite", [LandingController::class, "getFavorite"]);
-        Route::get("/search/{name}", [LandingController::class, "search"]);
         Route::get("/getUser/{id}", [LandingController::class, "getUser"]);
-        Route::get("/getMessage", [LandingController::class, "getMessage"]);
+        Route::get("/search/{name}", [LandingController::class, "search"]);
+        Route::get("/getFavorite", [LandingController::class, "getFavorite"]);
+        Route::get("/replies/{receiver_id}", [LandingController::class, "replies"]);
+        Route::get("/viewProfile/{email}", [LandingController::class, "viewProfile"]);
+
+
+        Route::post("/updateReply", [LandingController::class, "updateReply"]);
         Route::post("/addFavorite", [LandingController::class, "addFavorite"]);
-        Route::post("/updateUserProfile/{id}", [LandingController::class, "updateUserProfile"]);
-        Route::post("/sendMessage", [LandingController::class, "sendMessage"]);
+        Route::post("/deleteReply/{id}", [LandingController::class, "deleteReply"]);
+        Route::post("/sendMessage/{sender_id}", [LandingController::class, "sendMessage"]);
+        Route::post("/updateUserProfile/{data}", [LandingController::class, "updateUserProfile"]);
     });
+
 
     Route::post("/register", [AuthController::class, "register"]);
     Route::post("/login", [AuthController::class, "login"]);
-    Route::post("/checkCode/{code}", [AuthController::class, "checkCode"]);
+    Route::post("/checkCode/{confirmation_code}", [AuthController::class, "checkCode"]);
+    Route::post("/checkEmail/{Email}", [AuthController::class, "checkEmail"]);
 });
